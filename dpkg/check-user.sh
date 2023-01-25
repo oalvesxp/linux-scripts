e0caaa08cf9d22318891f46620062a93bb21a475
Osvaldo Alves <oalvesxp@gmail.com>
2023-01-25 15:31:19 -0300

    Finished check-user.sh script

--- dpkg/check-user.sh
+++ dpkg/check-user.sh
@@ -4,6 +4,12 @@ me="$(whoami)"
 read -p "Digite o username: " user
 check="$(cat /etc/passwd | grep $user | cut -d ":" -f1)"
 
-echo $user
-echo $me
-echo $check
+if [ "$user" = "$me" ]; then
+	echo "O usuário $me está logado."
+	cat /etc/passwd | grep $me
+ elif [ "$user" = "$check" ]; then 
+	echo "O usuário $user existe."
+	cat /etc/passwd | grep $user
+ else
+	echo "O usuário não existe."
+fi
