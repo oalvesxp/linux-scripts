8d0c4073060e3e10b48c8ad154c2ae42178036fe
Osvaldo Alves <oalvesxp@gmail.com>
2023-01-25 16:02:09 -0300

    finishing the check-httpd.sh

--- dpkg/check-httpd.sh
+++ dpkg/check-httpd.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 check_url=$(curl --write-out %{http_code} --silent --output /dev/null www.google.com)
-echo $check_url
+
+if [ $check_url -ne 200 ]; then
+    echo "Houve um problema com o servidor, tentando reiniciá-lo  $(date +%F\ %T)" >> /var/log/issues-httpd.log
+    systemctl restart httpd
+fi
