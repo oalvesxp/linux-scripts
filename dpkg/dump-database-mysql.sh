232e99f9246da1ee5de7d1cc05a8e4ef05ee27d6
Osvaldo Alves <oalvesxp@gmail.com>
2023-01-25 16:59:49 -0300

    adding mysql command to generate dump, and inserting check to dump file.

--- dpkg/dump-database-mysql.sh
+++ dpkg/dump-database-mysql.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-read -p "Insert your name: " name
 read -p "Insert the database name: " db
 read -p "Insert the database user: " dbuser
+read -p "Insert the user password: " dbpasswd
 read -p "Insert the host IP from database: " host
 
-echo $name $db $dbuser $host
+mysqldump --single-transaction -h$host $db -u$dbuser -p$dbpasswd | gzip -9 $HOME/$db-`date +%d-%m-%y`.sql.gz
+zcat $HOME/$db-`date +%d-%m-%y`.sql.gz | tail -n1
+
+echo "Take your dump on $HOME/$db-`date +%d-%m-%y`.sql.gz" 
