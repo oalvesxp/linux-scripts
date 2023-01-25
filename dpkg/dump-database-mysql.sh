fcb2c40ceb372623d2892991a31e0d10ba142be6
Osvaldo Alves <oalvesxp@gmail.com>
2023-01-25 16:48:26 -0300

    Insert variables to save input values from database and user.

--- dpkg/dump-database-mysql.sh
+++ dpkg/dump-database-mysql.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+read -p "Insert your name: " name
+read -p "Insert the database name: " db
+read -p "Insert the database user: " dbuser
+read -p "Insert the host IP from database: " host
+
+echo $name $db $dbuser $host
