e0a5d707e71c88d2c778454f13034a2a6e99e133
Osvaldo Alves <oalvesxp@gmail.com>
2023-01-29 10:54:25 -0300

    updating the mysqldump script

--- dpkg/dump-database-mysql.sh
+++ dpkg/dump-database-mysql.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+set -e
+
 read -p "Insert the database name: " db
 read -p "Insert the database user: " dbuser
 read -p "Insert the user password: " dbpasswd
 read -p "Insert the host IP from database: " host
 
-mysqldump --single-transaction -h$host $db -u$dbuser -p$dbpasswd | gzip -9 $HOME/$db-`date +%d-%m-%y`.sql.gz
-zcat $HOME/$db-`date +%d-%m-%y`.sql.gz | tail -n1
+filename=$db-`date +%d-%m-%y`.sql.gz
+dumpdir=$HOME
 
-echo "Take your dump on $HOME/$db-`date +%d-%m-%y`.sql.gz" 
+sudo mysqldump --single-transaction --verbose -h$host $db -u$dbuser -p$dbpasswd | gzip -9 > $dumpdir/$filename
+zcat $dumpdir/$filename | tail -n1
+
+echo "Take your dump on $dumpdir/$filename"
