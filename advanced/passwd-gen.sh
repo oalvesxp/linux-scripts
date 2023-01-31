fd1eea49bb70a49aabf8b47ee7cccf05555bd540
Osvaldo Alves <oalvesxp@gmail.com>
2023-01-31 15:52:15 -0300

    improving the passwd-gen script

--- advanced/passwd-gen.sh
+++ advanced/passwd-gen.sh
@@ -6,11 +6,27 @@
 # @description: Script to genenrate the security password.
 # @use: $ sh to-extract.sh <number-of-characters>
 
+passwdGen(){
+   echo "Password without special characters:"
+   less -f /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c"${1:-48}"
+   echo "\n"
 
-if [ $# -eq 1 ]; then
-    less -f /dev/urandom | tr -dc 'a-zA-Z0-9-_!@#$%^&*()_+{}|:<>?=' | head -c"${1:-$1}"
-    echo "\n"
-else
-    less -f /dev/urandom | tr -dc 'a-zA-Z0-9-_!@#$%^&*()_+{}|:<>?=' | head -c"${1:-48}"
-    echo "\n"
+   echo "Password with special characters:"
+   less -f /dev/urandom | tr -dc 'a-zA-Z0-9-_!@#$%^&*()_+{}|:<>?=' | head -c"${1:-$1}"
+   echo "\n"
+}
+
+if [ $# -eq 0 ]; then
+   echo "No arguments provided..."
+   echo "Usage: sh passwd-gen.sh <number-of-characters>"
+   echo "       sh passwd-gen.sh 12"
+   exit 1
 fi
+
+while [ $# -gt 0 ]
+do
+   echo "Generating passwords... \n"
+   sleep 1
+   passwdGen "$1"
+   shift 1
+done
